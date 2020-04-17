b6f26dc02b18f2b2f0e36afabbb77355af4504d5
Shane Kaestner <shane.kaestner@yahoo.com>
2020-04-17 10:17:36 -0700

    all of the characters are showing on the screen.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -2,16 +2,18 @@ import React, { useState, useEffect } from "react";
 import './App.css';
 import axios from 'axios'
 import styled from 'styled-components'
+import Character from './components/Character'
 
-const url = 'https://swapi.py4e.com/api/'
+const url = 'https://swapi.py4e.com/api/people/'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [characterData, setCharacterData] = useState([])
   useEffect(() => {
     axios.get(`${url}`)
     .then(res =>{
-      console.log(res)
+      setCharacterData(res.data.results)
     })
     .catch(err => {
       console.log(err)
@@ -23,9 +25,12 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  console.log('characterData is ',characterData)
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {characterData.map((event) => <Character key={event.name} character={event}/>)}
+      {/* <Character characterData={characterData}/> */}
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,25 @@
 // Write your Character component here
+import React, { useState, useEffect  } from "react";
+
+
+const Character = (props) => {
+    const [films, setFilms] = useState('')//map
+    const [name, setName] = useState('')
+    const [birthYear, setBirthYear] = useState(null)
+    {console.log("props in character",props)}
+    useEffect(() => {
+        setName(props.character.name)
+        setBirthYear(props.character.birth_year)
+        setBirthYear(props.character.name)
+        },
+      []);
+    return (
+        <div className = "character">
+            <h1 className = "character-title">{name}</h1>
+            <h4>{birthYear}</h4>
+           
+        </div>
+    )
+}
+
+export default Character;
