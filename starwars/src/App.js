815f7f85dfc05335852bb03010c4b212583edace
Shane Kaestner <shane.kaestner@yahoo.com>
2020-04-17 08:38:46 -0700

    now successfully pulling the api data.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import './App.css';
+import axios from 'axios'
+import styled from 'styled-components'
+
+const url = 'https://swapi.py4e.com/api/'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+  useEffect(() => {
+    axios.get(`${url}`)
+    .then(res =>{
+      console.log(res)
+    })
+    .catch(err => {
+      console.log(err)
+      debugger
+    })
+    },
+  []);
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
